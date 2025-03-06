b3a76aaf8d58b3c4aed8a3746f070450e3b10f66
serwes <serwesbhatta@gmail.com>
2025-03-06 12:31:09 -0600

    feat: setup backend

---
+++ backend/app/config.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres@localhost/roommate_app"
+
+# Create a database engine
+engine = create_engine(DATABASE_URL)
+
+# Create a SessionLocal class for database sessions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
