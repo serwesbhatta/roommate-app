9adffc8372e1a46b8766edfbec649a61db2607c2
Sangam Lamichhane <156962466+Magnas55@users.noreply.github.com>
2025-03-20 04:12:47 -0500

    Added User profile, Admin pages, etc.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,6 @@
 export {default as LandingPage} from './LandingPage';
 export {default as Login} from './auth/Login.jsx';
 export {default as Signup} from './auth/Signup.jsx';
+export {default as Admin} from './admin/Admin.jsx';
+export {default as User} from './user/User.jsx';
+export {default as NotFound} from './NotFound.jsx';
